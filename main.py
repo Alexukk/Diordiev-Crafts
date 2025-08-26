a9bdad77911abccddeff0969ac0d95174cc3afc9
Alex <alexukrok@gmail.com>
2025-08-26 16:06:55 +0300

    Main page and about page created, routes are set

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask, redirect, render_template, request
 import telebot
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DiordievCrafts.db'
+db = SQLAlchemy(app)
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+@app.route('/about')
+def aboutUs():
+    return render_template('about.html')
 
 
 
